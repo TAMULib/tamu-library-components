af0be0847f5389aedd12395533e74ca4c0036818
jeremythuff <jeremythuff@gmail.com>
2020-01-28 18:19:58 -0600

    WIP setting font color

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN npm install
 RUN npm run build
 
 FROM httpd:2.4-alpine
-COPY ./dist/tl-components.js /usr/local/apache2/htdocs/
+COPY ./dist/tl-components.js /usr/local/apache2/htdocs/tl-components
--- src/app/tl-header/tl-header.component.ts
+++ src/app/tl-header/tl-header.component.ts
@@ -10,5 +10,7 @@ export class TamuHeaderComponent {
   logoSrc = "https://library.tamu.edu/assets/bootstrap/images/tam-lg.png";
   logoText = " Texas A&M University Libraries";
   primaryColor = "#500000";
+  secondaryColor = "#3c0001";
+  grayColor = "#ededed";
   @Input() pageTitle = "TL Header Component";
 }
