8df677a0aab5ae1819483b10512c9a1da2b45020
William Welling <wwelling@library.tamu.edu>
2020-02-02 09:46:15 -0600

    keep tl_components files after building

--- scripts/build-tl-components.js
+++ scripts/build-tl-components.js
@@ -1,16 +1,18 @@
-const fs = require('fs-extra');
+// const fs = require('fs-extra');
 const concat = require('concat');
 
 (async function build() {
   const files = [
     'dist/tl-components/runtime-es2015.js',
     'dist/tl-components/polyfills-es2015.js',
+    // 'dist/tl-components/styles-es2015.js',
     'dist/tl-components/scripts.js',
+    // 'dist/tl-components/vendor-es2015.js',
     'dist/tl-components/main-es2015.js'
   ];
 
   await concat(files, 'dist/tl-components.js');
 
-  await fs.remove("dist/tl-components");
+  // await fs.remove("dist/tl-components");
 
 })();
