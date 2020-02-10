e46448ea470f97e42e274b69abe5d13fb8e1ca29
William Welling <wwelling@library.tamu.edu>
2020-02-10 14:16:46 -0600

    update tslint with selector lint rules

--- src/app/tl-nav-list/tl-nav-list.component.ts
+++ src/app/tl-nav-list/tl-nav-list.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 import { Alignment } from '@wvr/elements';
 
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'tl-nav-list-element',
   templateUrl: './tl-nav-list.component.html',
   styleUrls: ['./tl-nav-list.component.scss']
