9bebd3fded16a73580b035b7971a7dca623f64ec
Rincy Mathew <rmathew@library.tamu.edu>
2020-05-08 12:15:22 -0500

    Conditional font styling for tl-components

--- src/app/shared/tl-abstract-base.component.ts
+++ src/app/shared/tl-abstract-base.component.ts
@@ -2,10 +2,10 @@ import { HostBinding, Input } from '@angular/core';
 
 export abstract class TamuAbstractBaseComponent {
   /** Allows for the override of the --tl-default-font-size css variable. */
-  @HostBinding('style.--tl-font-size') _fontSize; // (this.inheritFontStyle === 'false') ? '23px' : 'inherit';
+  @HostBinding('style.--tl-font-size') _fontSize = 'var(--tl-default-font-size)';
 
   /** Allows for the override of the --tl-font-family-sans-serif css variable. */
-  @HostBinding('style.--tl-font-family-sans-serif') _fontFamily; // = (this.inheritFontStyle === 'false') ? 'sans-serif' : 'inherit';
+  @HostBinding('style.--tl-font-family-sans-serif') _fontFamily = 'var(--tl-default-font-family-sans-serif)';
 
   @Input() set inheritFontStyle(value: 'true' | 'false') {
     this._fontSize = (value === 'true') ? 'inherit' : 'var(--tl-default-font-size)';
--- src/app/tl-header/tl-header.component.ts
+++ src/app/tl-header/tl-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Link } from '../shared/link';
 import { TamuAbstractBaseComponent } from '../shared/tl-abstract-base.component';
 
@@ -37,22 +37,11 @@ export class TamuHeaderComponent extends TamuAbstractBaseComponent {
   /** This defines the TAMU page  header title and is displayed as page title. */
   @Input() pageTitle = 'TL Header Component';
 
-  // /** Allows for the override of the --tl-default-font-size css variable. */
-  // @HostBinding('style.--tl-font-size') _fontSize; // (this.inheritFontStyle === 'false') ? '23px' : 'inherit';
-
-  // /** Allows for the override of the --tl-font-family-sans-serif css variable. */
-  // @HostBinding('style.--tl-font-family-sans-serif') _fontFamily; // = (this.inheritFontStyle === 'false') ? 'sans-serif' : 'inherit';
-
-  // @Input() set inheritFontStyle(value: 'true' | 'false') {
-  //   this._fontSize = (value === 'true') ? 'inherit' : 'var(--tl-default-font-size)';
-  //   this._fontFamily = (value === 'true') ? 'inherit' : 'var(--tl-default-font-family-sans-serif)';
-  //   console.log(value, ': ', this._fontFamily, this._fontSize);
-  // }
-
   /** Used to iterate the header navigation list. */
   topLinks: Array<Link> = [
     { href: 'https://library.tamu.edu/about/hours.html', value: 'Hours' },
     { href: 'https://library.tamu.edu/about/index.html', value: 'Libraries' },
     { href: 'http://askus.library.tamu.edu', value: 'AskUs' }
   ];
+
 }
