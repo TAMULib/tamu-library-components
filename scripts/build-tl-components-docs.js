19879389f378c37f8794aecc3c66e248d6394cbf
Rincy Mathew <rmathew@library.tamu.edu>
2020-06-02 09:51:46 -0500

    WIP - addded server-static dependency in package.json file

--- scripts/build-tl-components-docs.js
+++ scripts/build-tl-components-docs.js
@@ -2,5 +2,5 @@ const fs = require('fs-extra');
 
 (async function build() {
   fs.ensureDir("docs");
-  fs.copy("index-doc.html", "docs/index.html");
+  fs.copy("index-docs.html", "docs/index.html");
 })();
