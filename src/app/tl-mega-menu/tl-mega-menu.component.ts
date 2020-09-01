3e805f4169e1fbd927dca31e28b0676fc68722ed
Rincy Mathew <rmathew@library.tamu.edu>
2020-09-01 17:13:27 -0500

    View All button text value can be customized.

--- src/app/tl-mega-menu/tl-mega-menu.component.ts
+++ src/app/tl-mega-menu/tl-mega-menu.component.ts
@@ -12,6 +12,19 @@ export class TlMegaMenuComponent extends TamuAbstractBaseComponent {
 
   @Input() viewAllHref = 'https://library.tamu.edu';
 
+  /** This variable allows customizing the text value of View All button in the mega menu. */
+  private _viewAllButtonText;
+
+  /** This allows overriding the button text value. */
+  @Input() set viewAllButtonText(value: string) {
+    this._viewAllButtonText = (value) ? value : `View All ${this.menuTitle}`;
+  }
+
+  /** This getter returns the customized View All button text value. */
+  get viewAllButtonText(): string {
+    return this._viewAllButtonText;
+  }
+
   // tslint:disable-next-line:unnecessary-constructor
   constructor(injector: Injector) {
     super(injector);
