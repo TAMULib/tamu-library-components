d53d5bb0478dcb77e4e7c4ccce594103b9dff9d1
Rincy Mathew <rmathew@library.tamu.edu>
2020-09-24 20:20:52 -0500

    Increased tick timimg in tl-mega-menu-component

--- src/app/tl-mega-menu/tl-mega-menu.component.spec.ts
+++ src/app/tl-mega-menu/tl-mega-menu.component.spec.ts
@@ -126,7 +126,7 @@ describe('MegaMenuComponent', () => {
     expect(component.menuXOffset)
       .toBe(0);
 
-    tick(1000);
+    tick(5000);
 
     expect(component.menuXOffset)
         .toBeGreaterThan(0);
