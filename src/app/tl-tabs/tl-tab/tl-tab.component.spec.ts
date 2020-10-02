ba80e1d159132fa0c6eb6a751f0df30520721c00
jeremythuff <jeremythuff@gmail.com>
2020-10-02 14:50:17 -0500

    Fixed tests

--- src/app/tl-tabs/tl-tab/tl-tab.component.spec.ts
+++ src/app/tl-tabs/tl-tab/tl-tab.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TlTabComponent } from './tl-tab.component';
 
@@ -8,6 +9,7 @@ describe('TlTabComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule],
       declarations: [ TlTabComponent ]
     })
     .compileComponents();
--- src/app/tl-tabs/tl-tabs.component.spec.ts
+++ src/app/tl-tabs/tl-tabs.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TlTabsComponent } from './tl-tabs.component';
 
@@ -8,6 +9,7 @@ describe('TlTabsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule],
       declarations: [ TlTabsComponent ]
     })
     .compileComponents();
