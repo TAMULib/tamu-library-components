21b5a1dd1af795a620043b5cde9be562503ab721
Rincy Mathew <rmathew@library.tamu.edu>
2020-10-05 16:25:14 -0500

    WIP added closeTimer input property

--- src/app/tl-alert/tl-alert.component.ts
+++ src/app/tl-alert/tl-alert.component.ts
@@ -9,10 +9,24 @@ import { TamuAbstractBaseComponent } from '../shared/tl-abstract-base.component'
 export class TlAlertComponent extends TamuAbstractBaseComponent {
 
   /** Used to override the alert class. */
-  @Input() alertClass;
+  @Input() alertClass: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' = 'primary';
 
   /** Used to override the type of alert.  */
-  @Input() alertType;
+  @Input() alertType: 'basic' | 'self-closing' | 'custom' =  'basic';
+
+  /** Used to override if close button has to be present for an alert.  */
+  @Input() closable: 'true' | 'false' = 'true';
+
+  private _closeTimer = 7000;
+
+  @Input() set closeTimer(value: number) {
+    this._closeTimer = value;
+  }
+
+  get closeTimer(): number {
+    // tslint:disable-next-line: radix
+    return this._closeTimer;
+  }
 
   // tslint:disable-next-line:unnecessary-constructor
   constructor(injector: Injector) {
