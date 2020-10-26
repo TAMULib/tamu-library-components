36916b266fa00e95d54a468d5a13e2a9350b8811
Rincy Mathew <rmathew@library.tamu.edu>
2020-10-26 14:00:40 -0500

    Work in Progress - tl-card

---
+++ src/app/tl-card/tl-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TlCardComponent } from './tl-card.component';
+
+describe('TlCardComponent', () => {
+  let component: TlCardComponent;
+  let fixture: ComponentFixture<TlCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TlCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TlCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
