155ed2a1c925b3bb7e03c7c9ed053a1809f233a5
Rincy Mathew <rmathew@library.tamu.edu>
2020-10-27 09:33:34 -0500

    Work In Progress - Tl-panel component

--- src/app/tl-lib.module.ts
+++ src/app/tl-lib.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { WvrLibModule } from '@wvr/elements';
 import { TlAlertComponent } from './tl-alert/tl-alert.component';
 import { TlButtonComponent } from './tl-button/tl-button.component';
+import { TlCardComponent } from './tl-card/tl-card.component';
 import { TlDropDownComponent } from './tl-drop-down/tl-drop-down.component';
 import { TamuFooterComponent } from './tl-footer/tl-footer.component';
 import { TamuHeaderComponent } from './tl-header/tl-header.component';
@@ -12,10 +13,9 @@ import { TamuItWorksComponent } from './tl-it-works/tl-it-works.component';
 import { TlMegaMenuSectionComponent } from './tl-mega-menu/tl-mega-menu-section/tl-mega-menu-section.component';
 import { TlMegaMenuComponent } from './tl-mega-menu/tl-mega-menu.component';
 import { TamuNavListComponent } from './tl-nav-list/tl-nav-list.component';
+import { TlPanelComponent } from './tl-panel/tl-panel.component';
 import { TlTabsComponent } from './tl-tabs/tl-tabs.component';
 import { TlTabComponent } from './tl-tabs/tl-tab/tl-tab.component';
-import { TlCardComponent } from './tl-card/tl-card.component';
-
 
 /** This property contains a list of TAMU components and the selector tags. */
 const elements = [
@@ -30,6 +30,7 @@ const elements = [
   { component: TlMegaMenuComponent, selector: 'tl-mega-menu' },
   { component: TlMegaMenuSectionComponent, selector: 'tl-mega-menu-section' },
   { component: TamuNavListComponent, selector: 'tl-nav-list' },
+  { component: TlPanelComponent, selector: 'tl-panel' },
   { component: TlTabsComponent, selector: 'tl-tabs' },
   { component: TlTabComponent, selector: 'tl-tab' }
 ];
@@ -47,6 +48,7 @@ const components = [
   TlMegaMenuComponent,
   TlMegaMenuSectionComponent,
   TamuNavListComponent,
+  TlPanelComponent,
   TlTabsComponent,
   TlTabComponent
 ];
