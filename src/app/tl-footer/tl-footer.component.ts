98575bd16d6883019f8fcca51bd4f1285aa3fa07
Rincy Mathew <rmathew@library.tamu.edu>
2020-12-01 16:14:42 -0600

    Removed backgroundColor from tl-footer

--- src/app/tl-footer/tl-footer.component.ts
+++ src/app/tl-footer/tl-footer.component.ts
@@ -14,9 +14,6 @@ import { TamuAbstractBaseComponent } from '../shared/tl-abstract-base.component'
 })
 export class TamuFooterComponent extends TamuAbstractBaseComponent {
 
-  /** Used to override the background color. */
-  backgroundColor = 'var(--tl-primary)';
-
   /** Used to iterate the footer navigation list. */
   links: Array<Link> = [
     { href: 'https://howdy.tamu.edu/uPortal/normal/render.uP', value: 'howdy.tamu.edu' },
