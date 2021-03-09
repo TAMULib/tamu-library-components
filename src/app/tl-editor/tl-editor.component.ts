f8d50ceda166f50303518e533617569592559713
Rincy Mathew <rmathew@library.tamu.edu>
2021-03-09 13:30:59 -0600

    WIP test editor

--- src/app/tl-editor/tl-editor.component.ts
+++ src/app/tl-editor/tl-editor.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectionStrategy, Component, Injector, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Injector } from '@angular/core';
 import { TamuAbstractBaseComponent } from '../shared/tl-abstract-base.component';
 
 @Component({
   selector: 'tl-editor-component',
   templateUrl: './tl-editor.component.html',
   styleUrls: ['./tl-editor.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TlEditorComponent extends TamuAbstractBaseComponent {
 
-  @Input() initialValue = 'This is tl-editor';
+  initialValue = 'This is tl-editor';
+
+  skinUrl =  `src/assets/skins/ui/oxide-dark`;
 
   // tslint:disable-next-line:unnecessary-constructor
   constructor(injector: Injector) {
