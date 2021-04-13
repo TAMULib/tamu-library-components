ad6988a0df4f107145931ff952d4af3ae44fe80f
Rincy Mathew <rmathew@library.tamu.edu>
2021-04-13 15:39:06 -0500

    WIP test edit

--- src/app/tl-themes/tl-themes.component.spec.ts
+++ src/app/tl-themes/tl-themes.component.spec.ts
@@ -1,13 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideMockStore } from '@ngrx/store/testing';
+import { APP_CONFIG, testAppConfig } from '@wvr/elements';
 import { TlThemesComponent } from './tl-themes.component';
 
 describe('TlThemesComponent', () => {
   let component: TlThemesComponent;
   let fixture: ComponentFixture<TlThemesComponent>;
+  const initialState = { theme: {
+    themes: {}
+  }};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule
+      ],
+      providers: [
+        provideMockStore({initialState}),
+        {
+          provide: APP_CONFIG,
+          useValue: testAppConfig
+        }
+      ],
       declarations: [ TlThemesComponent ]
     })
     .compileComponents();
@@ -20,6 +35,10 @@ describe('TlThemesComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component)
+      .toBeTruthy();
   });
+
 });
+
+
--- src/app/tl-themes/tl-themes.component.ts
+++ src/app/tl-themes/tl-themes.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 // import * as themes from '';
 
 @Component({
-  selector: 'tl-tl-theme',
-  templateUrl: './tl-theme.component.html',
-  styleUrls: ['./tl-theme.component.scss']
+  selector: 'tl-themes',
+  templateUrl: './tl-themes.component.html',
+  styleUrls: ['./tl-themes.component.scss']
 })
 export class TlThemesComponent implements OnInit {
 
