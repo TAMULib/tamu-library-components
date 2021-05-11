95f7651f6d7d30820d9a4a6156af8349068c0839
William Welling <wwelling@library.tamu.edu>
2021-05-11 14:09:10 -0500

    update wysiwyg change detection strategy

--- projects/tl-elements/src/lib/tl-wysiwyg/tl-wysiwyg.component.ts
+++ projects/tl-elements/src/lib/tl-wysiwyg/tl-wysiwyg.component.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectionStrategy, Component, Injector, Input } from '@angular/core';
+import { Component, Injector, Input } from '@angular/core';
 import { TamuAbstractBaseComponent } from '../utility/tl-abstract-base.component';
 
 @Component({
   selector: 'tl-wysiwyg-component',
   templateUrl: './tl-wysiwyg.component.html',
-  styleUrls: ['./tl-wysiwyg.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./tl-wysiwyg.component.scss']
 })
 export class TlWysiwygComponent extends TamuAbstractBaseComponent {
 
