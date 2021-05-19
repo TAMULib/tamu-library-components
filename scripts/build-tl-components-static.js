f3a620f303a14189a703e65a03aa1057ac98c05d
William Welling <wwelling@library.tamu.edu>
2021-05-19 12:37:54 -0500

    update usage doc configs

--- scripts/build-tl-components-static.js
+++ scripts/build-tl-components-static.js
@@ -1,31 +1,29 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
-
 const staticPath = 'static';
-const tamuComponentsPath = 'static/tamu-library-components';
-const docPath = `${tamuComponentsPath}/docs`;
-const reportPath = `${tamuComponentsPath}/reports`;
-const basePath = 'dist/bundle';
+const tamuLibraryComponentsPath = 'static/tamu-library-components';
+const docPath = `${tamuLibraryComponentsPath}/docs`;
+const reportPath = `${tamuLibraryComponentsPath}/reports`;
 
 (async function build() {
   fs.ensureDir(`${docPath}/`);
   fs.ensureDir(`${reportPath}`);
 
-  fs.copy("src/index-static.html", `${tamuComponentsPath}/index.html`);
-  fs.copy("src/assets", `${tamuComponentsPath}/assets`);
-  fs.copy(basePath, `${tamuComponentsPath}/${basePath}`);
+  fs.copy("src/index-static.html", `${tamuLibraryComponentsPath}/index.html`);
+  fs.copy("src/assets", `${tamuLibraryComponentsPath}/assets`);
 
   fs.copy("src/index-static.html", `${staticPath}/index.html`);
   fs.copy("src/assets", `${staticPath}/assets`);
-  fs.copy(basePath, `${staticPath}/${basePath}`);
 
   fs.copy("src/index-docs.html", `${docPath}/index.html`);
   fs.copy("src/assets", `${docPath}/assets`);
-  fs.copy(basePath, `${docPath}/${basePath}`);
 
   fs.copy("src/index-reports.html", `${reportPath}/index.html`);
   fs.copy("src/assets", `${reportPath}/assets`);
-  fs.copy(basePath, `${reportPath}/${basePath}`);
+
+  if( fs.existsSync("dist/bundle/assets/tinymce") ) {
+    fs.copy("dist/bundle/assets/tinymce", `${docPath}/usage/assets/tinymce`);
+  }
 
 })();
