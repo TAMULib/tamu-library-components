9650d31563abf8c56177b31e907cadf8547d11d2
William Welling <wwelling@library.tamu.edu>
2021-05-20 16:24:36 -0500

    upgrade angular and other dependencies

--- projects/tl-elements/src/test.ts
+++ projects/tl-elements/src/test.ts
@@ -1,12 +1,12 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
+// tslint:disable-next-line:no-import-side-effect
+import '@angular/localize/init';
+/* tslint:disable: no-import-side-effect */
+import 'zone.js';
+import 'zone.js/testing';
+/* tslint:enable: no-import-side-effect */
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
