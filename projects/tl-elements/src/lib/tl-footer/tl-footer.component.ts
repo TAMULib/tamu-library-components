18959487a3340c4c35343f81dca7b85806cd7f06
Jeremy Huff <jeremythuff@gmail.com>
2021-09-08 14:18:36 -0500

    Added support for login button on footer

--- projects/tl-elements/src/lib/tl-footer/tl-footer.component.ts
+++ projects/tl-elements/src/lib/tl-footer/tl-footer.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Injector, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject, Injector, Input, ViewEncapsulation } from '@angular/core';
+import { AppConfig, APP_CONFIG } from '@wvr/elements';
 import { Link } from '../utility/link';
 import { TamuAbstractBaseComponent } from '../utility/tl-abstract-base.component';
 
@@ -13,7 +14,9 @@ import { TamuAbstractBaseComponent } from '../utility/tl-abstract-base.component
   encapsulation: ViewEncapsulation.ShadowDom,
   changeDetection: ChangeDetectionStrategy.Default
 })
-export class TamuFooterComponent extends TamuAbstractBaseComponent {
+export class TamuFooterComponent extends TamuAbstractBaseComponent implements AfterViewInit {
+
+  @Input() loginUrl: string;
 
   /** Used to iterate the footer navigation list. */
   links: Array<Link> = [
@@ -32,6 +35,15 @@ export class TamuFooterComponent extends TamuAbstractBaseComponent {
   // tslint:disable-next-line:unnecessary-constructor
   constructor(injector: Injector) {
     super(injector);
+  }
+
+  ngAfterViewInit(): void {
+    if (!!this.loginUrl) {
+      this.links.push({
+          href: this.loginUrl,
+          value: 'Login'
+      });
+    }
   }
 
 }
