e3a6c980b4dd19e4667535976dec40fbe2226943
Rincy Mathew <rmathew@tamu.edu>
2021-10-19 14:55:10 -0500

    Added option for toolbar and height

--- projects/tl-elements/src/lib/tl-wysiwyg/tl-wysiwyg.component.ts
+++ projects/tl-elements/src/lib/tl-wysiwyg/tl-wysiwyg.component.ts
@@ -14,6 +14,10 @@ export class TlWysiwygComponent extends TamuAbstractBaseComponent {
 
   skin = 'oxide';
 
+  toolbar = 'undo redo | bold italic removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | help cancel save';
+
+  @Input() height = '500';
+
   // tslint:disable-next-line:unnecessary-constructor
   constructor(injector: Injector) {
     super(injector);
